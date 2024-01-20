8c7ae858b5f8090f7f82c559abf9d09f14f89313
Juan Albornoz <jualbornozc@gmail.com>
2024-01-20 00:06:22 -0300

    Creando interfaz grafica con flet, para el algortimo de ordenamiento por mezcla

--- mezcla.py
+++ mezcla.py
@@ -1,6 +1,25 @@
 import flet as ft
+import random
 
 def main(page: ft.Page):
-    pass
+    
+    def create_containers(number):
+        container_list = []
+        for _ in range(number):
+            container_list.append(
+                ft.Container(
+                    content = ft.Text(value=random.randint(1, 100)),
+                    alignment = ft.alignment.center,
+                    width=50,
+                    height=50, 
+                    bgcolor=ft.colors.ORANGE,
+                    border_radius=25
+                )
+            )
+        return container_list
+
+    row = ft.Row(controls=create_containers(10))
+    page.add(row)
+                    
 
 ft.app(target=main)
